bb533bbeb81f12c3fb5b1670fdeabf865246f388
Napoleon Arouldas <meetneps@gmail.com>
2019-06-22 16:53:02 +0530

    Working now

--- pascal-programs/histogram.pas
+++ pascal-programs/histogram.pas
@@ -15,28 +15,45 @@ program histogram;
 }
 
 var a,h: array[0..100] of integer;
-	i,m,n: integer;
+	i,j,m,n: integer;
 
 begin
 	writeln('How many students ? ');
 	read(n);
 
-	for i:= 0 to 100 do
+	for i:= 0 to 20 do
 		h[i] := 0;
 
-	for i:= 1 to n do 
+	for i:= 0 to n do 
 	begin
 		write('Enter mark: ');
-		read(m);
-        a[i] := m;
-		h[m] := h[m] + 1;
+		read(a[i]);
+        
 	end;
+    writeln('j := ':10,' a[j]:=  ':10, ' h[j] :=  ':10 );
+
+    for i:= 0 to 20 do
+    begin
+        for j:= 0 to n do
+        begin
+            if a[j] = i then
+		        h[i] := h[i] + 1;
+	      
+	    end;
+	  { writeln( i:10 ,a[i]:10,h[i]:10 ); }
+
+    end;
     
     writeln('Numbers':10, 'Frq.':5);
-	for i:= 0 to 100 do
+	for i:= 0 to 20 do
 	begin
 	     { if( h[i] > 0 ) then }
-		 writeln(a[i]:10,h[i]:5);
+		 write(i:10);
+
+		 for j:= 1 to h[i] do
+		    write('*':3);
+		 
+		 writeln;
 	end
 
 end.
