5e89b90b8bc9a35da43828e90eb5431a9ae6dcd3
Napoleon Arouldas <meetneps@gmail.com>
2019-06-22 14:02:59 +0530

    comments added

--- pascal-programs/histogram.pas
+++ pascal-programs/histogram.pas
@@ -1,4 +1,18 @@
 program histogram;
+{ 
+  Given input: An array of numbers say 12, 13, 11, 2, 15, 19, 22, 22, 22, 21, 11 
+  Output should be 
+  Numbers   Occurance
+  12		1
+  13		1
+  11		2
+  2         1
+  15        1
+  19        1
+  22        3
+  21        1
+	
+}
 
 var a,h: array[0..100] of integer;
 	i,m,n: integer;
